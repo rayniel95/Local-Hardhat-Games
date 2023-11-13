379d3757b2ae19a13a347a1a69b93fac45a79597
rayniel95 <rainyel.ramos@gmail.com>
2023-11-13 18:09:59 -0500

    done

--- scripts/win5.js
+++ scripts/win5.js
@@ -1,12 +1,18 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
+    const setXTx = await game.giveMeAllowance(10000)
+    await setXTx.wait()
+
+    const setYTx = await game.mint(10000)
+    await setYTx.wait()
+
     const tx = await game.win();
 
     // did you win? Check the transaction receipt!
